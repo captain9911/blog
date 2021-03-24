c83940f33874a6e6cfaf03842c3e61e33066e0a3
captain9911 <captain9911@163.com>
2021-03-24 11:05:09 +0800

    push js

--- _includes/scripts/page.js
+++ _includes/scripts/page.js
@@ -3,14 +3,14 @@
 })();*/
 
 (function(){
-    var bp = document.createElement('script');
-    var curProtocol = window.location.protocol.split(':')[0];
-    if (curProtocol === 'https'){
-   bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+  var bp = document.createElement('script');
+  var curProtocol = window.location.protocol.split(':')[0];
+  if (curProtocol === 'https'){
+    bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
   }
   else{
-  bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    bp.src = 'http://push.zhanzhang.baidu.com/push.js';
   }
-    var s = document.getElementsByTagName("script")[0];
-    s.parentNode.insertBefore(bp, s);
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(bp, s);
 })();
